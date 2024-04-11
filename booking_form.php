bc230ea9eab630bd70b13c951545c85a161a36a7
aaradhy25tiwari <aaradhy.tiwari2584@gmail.com>
2024-04-11 16:26:35 +0530

    made booking form work

---
+++ booking_form.php
@@ -0,0 +1,19 @@
+<?php
+    $connection = mysqli_connect('localhost', 'root','', 'book_db');
+    if(isset($_POST['send'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $address = $_POST['adress'];
+        $location  = $_POST['location'];
+        $guests = $_POST[ 'guests'];
+        $arrivals = $_POST['arrivals'];
+        $departure = $_POST['departures'];
+
+        $request = "insert into book_form(name, email, phone, address, location, guests, arrivals, departures) values ('$name', '$email', '$phone', '$address', '$location','$guests', '$arrivals', '$departure')";
+        mysqli_query($connection, $request);
+        header('location:book.php');
+    }else{
+        echo 'something went wrong, Try again';
+    }
+?>
